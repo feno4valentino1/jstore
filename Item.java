1d6b51b2b19661a2528bc74fd061bcf193b7f7bf
feno4valentino1 <feno.valentino@ui.ac.id>
2019-02-21 17:54:43 +0700

    modul 1
    modul 1

--- Item.java
+++ Item.java
@@ -19,52 +19,42 @@ public class Item
     {
 
     }
-
     public int getId()
     {
         return 0;
     }
-    
     public String getName()
     {
         return "";
     }
-    
     public int getStock()
     {
         return 0;
     }
-    
     public int getPrice()
     {
         return 0;
     }
-    
     public String getCategory()
     {
         return "";
     }
-    
     public int setId(int id)
     {
         return 0;
     }
-    
     public String setName(String name)
     {
         return "";
     }
-    
     public int setStock(int stock)
     {
         return 0;
     }
-    
     public int setPrice(int price)
     {
         return 0;
     }
-    
     public String setCategory(String category)
     {
         return "";
