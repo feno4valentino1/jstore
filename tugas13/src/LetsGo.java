76fd3560f27c6fcd4c9e0f3c89b8d0c7f8de4783
feno4valentino1 <feno.valentino@ui.ac.id>
2019-04-21 23:47:05 +0700

    cs modul 7 tugas 13
    cs modul 7 tugas 13

---
+++ tugas13/src/LetsGo.java
@@ -0,0 +1,28 @@
+import java.lang.Math.*;
+
+public class LetsGo {
+
+    public LetsGo()
+    {
+
+    }
+
+    public static int random()
+    {
+        int max = 500;
+        int min = 100;
+        int range = max - min + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
+    public static void main(String[] args)
+    {
+        RunForYourLife Lionel = new RunForYourLife("Lionel", 1);
+        RunForYourLife Andres = new RunForYourLife("Andres", 0.8);
+        RunForYourLife Messi = new RunForYourLife("Messi", 0.6);
+
+        Lionel.start();
+        Andres.start();
+        Messi.start();
+    }
+}
---
+++ tugas13/src/RunForYourLife.java
@@ -0,0 +1,65 @@
+import java.lang.Math.*;
+
+public class RunForYourLife implements Runnable {
+    private String name;
+    private int runDelay;
+    private double handicap;
+    private int checkpoint;
+    private Thread thread;
+
+    public RunForYourLife(String name, double handicap)
+    {
+        this.name = name;
+        this.handicap = handicap;
+        this.checkpoint = 1;
+    }
+
+    public void run()
+    {
+        System.out.println(name + ", go!");
+
+        while(getCheckpoint() < 20)
+        {
+            setRunDelay((int) Math.round(LetsGo.random() * handicap));
+
+            try
+            {
+                Thread.sleep(runDelay);
+                System.out.println(name + " has passed checkpoint (" +  checkpoint + ").");
+                checkpoint = checkpoint + 1;
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println(name + " was interrupted.");
+            }
+        }
+
+        System.out.println(name + " has finished!");
+    }
+
+    public void start()
+    {
+        if (thread == null)
+        {
+            thread = new Thread(this, getName());
+            System.out.println(name + ", ready.");
+            System.out.println(name + ", set...");
+            thread.start();
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getCheckpoint()
+    {
+        return checkpoint;
+    }
+
+    public void setRunDelay(int runDelay)
+    {
+        this.runDelay = runDelay;
+    }
+}
